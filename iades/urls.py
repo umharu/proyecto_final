3cf9c76e1d9a2ebea28899bce48fe89200952e55
umharu <maxiroldancapponi@gmail.com>
2024-12-12 17:30:15 -0300

    final commit

--- iades/urls.py
+++ iades/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 
 #from .views import cursos, estudiantes, profesores, inicio
 from .views import inicio, home, Cursos
+from . import views
+
 
 
 urlpatterns = [
     path("", inicio, name="Inicio"),
-    path("home/", home, name="home"),
     path("Cursos/", Cursos, name="Cursos"),
+    path('home/', views.home, name='home'),
+    
     #path("profesores/", profesores, name="Profesores"),
 
 ]
--- proyectoiades/urls.py
+++ proyectoiades/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from iades import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('iades/', include('iades.urls')),
+    path('home/', include('iades.urls')),
     
 ]
